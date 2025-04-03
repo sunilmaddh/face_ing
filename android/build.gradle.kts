b3b15d76318049873bbad9e9366fac505f20ced8
INDO SAKURA <indosakura@INDOs-Mac-mini.local>
2025-04-03 12:01:43 +0530

    made some changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,13 @@
+plugins {
+    kotlin("android") version "2.1.0" apply false
+}
+buildscript {
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.android.tools.build:gradle:8.1.4")
+    }
+}
 allprojects {
     repositories {
         google()
