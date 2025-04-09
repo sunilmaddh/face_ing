c3b208d758d8c1f446802e1f25ad641e3c0a22ae
INDO SAKURA <indosakura@INDOs-Mac-mini.local>
2025-04-09 09:45:07 +0530

    fixed some scroll issue in signup button

--- lib/modules/views/auth/singup_screen.dart
+++ lib/modules/views/auth/singup_screen.dart
@@ -31,91 +31,94 @@ class _SignupScreenState extends State<SignupScreen> {
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Stack(
           children: [
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: AppDimensions.height(30)),
-                  CustomFormField(
-                    validator: (email) {
-                      if (email == null || email.isEmpty) {
-                        return "Please enter email ID";
-                      }
-                      return null;
-                    },
-                    label: AppConstents.email,
-                    hint: AppConstents.emailHint,
-                    controller: _authController.emailSignController,
-                  ),
-                  SizedBox(height: 20),
-                  CustomFormField(
-                    obscureText: true,
-                    validator: (pass) {
-                      if (pass == null || pass.isEmpty) {
-                        return "Please enter password";
-                      }
-                      return null;
-                    },
-                    label: AppConstents.password,
-                    hint: AppConstents.passHint,
-                    controller: _authController.passwordSignController,
-                  ),
-                  SizedBox(height: 20),
-                  CustomFormField(
-                    validator: (confPass) {
-                      if (confPass == null || confPass.isEmpty) {
-                        return "Please enter confirm password";
-                      } else if (_authController.passwordSignController.text !=
-                          _authController.confirmPasswordController.text) {
-                        return "Confirm password should be same as password";
-                      }
-                      return null;
-                    },
-                    label: AppConstents.confirmPassword,
-                    hint: AppConstents.confPassHint,
-                    controller: _authController.confirmPasswordController,
-                  ),
-                  SizedBox(height: AppDimensions.height(10)),
-                  TermsCheckboxWidget(
-                    authController: _authController,
-                    message:
-                        "Accept to the legal terms by clicking on the check box to be able to provide services",
-                  ),
-                ],
+            Positioned.fill(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.only(bottom: 100),
+                child: Column(
+                  children: [
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: AppDimensions.height(30)),
+                          CustomFormField(
+                            validator: (email) {
+                              if (email == null || email.isEmpty) {
+                                return "Please enter email ID";
+                              }
+                              return null;
+                            },
+                            label: AppConstents.email,
+                            hint: AppConstents.emailHint,
+                            controller: _authController.emailSignController,
+                          ),
+                          SizedBox(height: 20),
+                          CustomFormField(
+                            obscureText: true,
+                            validator: (pass) {
+                              if (pass == null || pass.isEmpty) {
+                                return "Please enter password";
+                              }
+                              return null;
+                            },
+                            label: AppConstents.password,
+                            hint: AppConstents.passHint,
+                            controller: _authController.passwordSignController,
+                          ),
+                          SizedBox(height: 20),
+                          CustomFormField(
+                            validator: (confPass) {
+                              if (confPass == null || confPass.isEmpty) {
+                                return "Please enter confirm password";
+                              } else if (_authController
+                                      .passwordSignController
+                                      .text !=
+                                  _authController
+                                      .confirmPasswordController
+                                      .text) {
+                                return "Confirm password should be same as password";
+                              }
+                              return null;
+                            },
+                            label: AppConstents.confirmPassword,
+                            hint: AppConstents.confPassHint,
+                            controller:
+                                _authController.confirmPasswordController,
+                          ),
+                          SizedBox(height: AppDimensions.height(10)),
+                          TermsCheckboxWidget(
+                            authController: _authController,
+                            message:
+                                "Accept to the legal terms by clicking on the check box to be able to provide services",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Obx(
               () => Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox(
-                  height: AppDimensions.height(100),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: PrimaryButton(
-                          isLoading: _authController.isLoading.value,
-                          text: AppConstents.signUp,
-                          onPressed: () {
-                            // AppNavigation.to(AppRoutes.otpFieldWidget);
-                            if (_formKey.currentState!.validate()) {
-                              if (_authController.isChecked.value) {
-                                _authController.getSingUpOtp();
-                              } else {
-                                AppSnackbar.show(
-                                  title: "Term & Conditons",
-                                  message: "Please select term & conditions",
-                                );
-                              }
-                            }
-                          },
-                        ),
-                      ),
-                      SizedBox(height: AppDimensions.height(20)),
-                    ],
-                  ),
+                alignment: Alignment.bottomRight,
+                child: PrimaryButton(
+                  isLoading: _authController.isLoading.value,
+                  text: AppConstents.signUp,
+                  onPressed: () {
+                    // AppNavigation.to(AppRoutes.otpFieldWidget);
+                    if (_formKey.currentState!.validate()) {
+                      if (_authController.isChecked.value) {
+                        _authController.getSingUpOtp();
+                      } else {
+                        AppSnackbar.show(
+                          title: "Term & Conditons",
+                          message: "Please select term & conditions",
+                        );
+                      }
+                    }
+                  },
                 ),
               ),
             ),
