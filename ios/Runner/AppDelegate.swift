3d50b7ebb31a18c421d2472aa12dc0560f0a5ea4
INDO SAKURA <indosakura@INDOs-MacBook-Air.local>
2025-08-29 17:23:39 +0530

    fixed some issue

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,15 @@ import UIKit
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+            // Allow only specific actions
+            if action == #selector(cut(_:)) ||
+               action == #selector(copy(_:)) ||
+               action == #selector(paste(_:)) ||
+               action == #selector(selectAll(_:)) {
+                return super.canPerformAction(action, withSender: sender)
+            }
+            // Disable everything else (including Scan Text)
+            return false
+        }
 }
